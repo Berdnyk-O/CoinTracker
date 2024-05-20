108e3b517c05f743f818e0caea933e4402664fdb
Berdnyk-O <sacha.berdnuk@gmail.com>
2024-05-20 16:12:06 +0300

    open the assets page immediately

--- CoinTracker/ViewModels/MainWindowViewModel.cs
+++ CoinTracker/ViewModels/MainWindowViewModel.cs
@@ -5,7 +5,7 @@ namespace CoinTracker.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        private INavigationService _navigationService;
+        private INavigationService _navigationService = null!;
         public INavigationService Navigation
         {
             get => _navigationService;
@@ -24,10 +24,13 @@ namespace CoinTracker.ViewModels
         public MainWindowViewModel(INavigationService navigationService)
         {
             Navigation = navigationService;
+            
             NavigateToAssetsCommand = new RelayCommand(_ => Navigation.NavigateTo<AssetsViewModel>(), _ => true);
             NavigateToExchangesCommand = new RelayCommand(_=> Navigation.NavigateTo<ExchangesViewModel>(), _ => true);
             NavigateToRatesCommand = new RelayCommand(prop => { Navigation.NavigateTo<RatesViewModel>(); }, prop => true);
             NavigateToMarketsCommand = new RelayCommand(prop => { Navigation.NavigateTo<MarketsViewModel>(); }, prop => true);
+
+            Navigation.NavigateTo<AssetsViewModel>();
         }
     }
 }
