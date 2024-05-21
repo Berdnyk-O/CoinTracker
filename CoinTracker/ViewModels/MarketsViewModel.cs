a7784ba6b7b343eee223ec7c7ffab6fbe93dde9c
Berdnyk-O <sacha.berdnuk@gmail.com>
2024-05-21 15:15:49 +0300

    add texbox for search on all pages

--- CoinTracker/ViewModels/MarketsViewModel.cs
+++ CoinTracker/ViewModels/MarketsViewModel.cs
@@ -6,9 +6,31 @@ namespace CoinTracker.ViewModels
 {
     public class MarketsViewModel : ViewModelBase
     {
-        public ObservableCollection<Market> Markets { get; set; } = null!;
-
         private readonly ICoinCapService _coinCapService;
+
+        public ObservableCollection<Market> Markets { get; set; } = null!;
+        private ObservableCollection<Market> _filteredMarkets = null!;
+        public ObservableCollection<Market> FilteredMarkets
+        {
+            get => _filteredMarkets;
+            set
+            {
+                _filteredMarkets = value;
+                OnPropertyChanged(nameof(FilteredMarkets));
+            }
+        }
+
+        private string _seachName = string.Empty;
+        public string SearchName
+        {
+            get => _seachName;
+            set
+            {
+                _seachName = value;
+                FilterMarkets();
+                OnPropertyChanged(nameof(SearchName));
+            }
+        }
 
         public MarketsViewModel(ICoinCapService coinCapService)
         {
@@ -19,7 +41,22 @@ namespace CoinTracker.ViewModels
         private async Task LoadAssets()
         {
             Markets = await _coinCapService.GetMarkets();
+            FilteredMarkets = Markets;
             OnPropertyChanged(nameof(Markets));
+        }
+
+        private void FilterMarkets()
+        {
+            if (string.IsNullOrEmpty(_seachName))
+            {
+                FilteredMarkets = Markets;
+            }
+            else
+            {
+                FilteredMarkets = new ObservableCollection<Market>(
+                    Markets.Where(x => x.BaseId.Contains(
+                        _seachName, StringComparison.InvariantCultureIgnoreCase)));
+            }
         }
     }
 }
--- CoinTracker/ViewModels/RatesViewModel.cs
+++ CoinTracker/ViewModels/RatesViewModel.cs
@@ -6,9 +6,31 @@ namespace CoinTracker.ViewModels
 {
     public class RatesViewModel : ViewModelBase
     {
-        public ObservableCollection<Rate> Rates { get; set; } = null!;
-
         private readonly ICoinCapService _coinCapService;
+
+        public ObservableCollection<Rate> Rates { get; set; } = null!;
+        private ObservableCollection<Rate> _filteredRates = null!;
+        public ObservableCollection<Rate> FilteredRates
+        {
+            get => _filteredRates;
+            set
+            {
+                _filteredRates = value;
+                OnPropertyChanged(nameof(FilteredRates));
+            }
+        }
+
+        private string _seachName = string.Empty;
+        public string SearchName
+        {
+            get => _seachName;
+            set
+            {
+                _seachName = value;
+                FilterRates();
+                OnPropertyChanged(nameof(SearchName));
+            }
+        }
 
         public RatesViewModel(ICoinCapService coinCapService)
         {
@@ -19,7 +41,22 @@ namespace CoinTracker.ViewModels
         private async Task LoadAssets()
         {
             Rates = await _coinCapService.GetRates();
+            FilteredRates = Rates;
             OnPropertyChanged(nameof(Rates));
+        }
+
+        private void FilterRates()
+        {
+            if (string.IsNullOrEmpty(_seachName))
+            {
+                FilteredRates = Rates;
+            }
+            else
+            {
+                FilteredRates = new ObservableCollection<Rate>(
+                    Rates.Where(x => x.Symbol.Contains(
+                        _seachName, StringComparison.InvariantCultureIgnoreCase)));
+            }
         }
     }
 }
