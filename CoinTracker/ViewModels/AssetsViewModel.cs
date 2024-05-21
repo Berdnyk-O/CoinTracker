57016b187065e4e9b1bdf9dfb8d3d3fe298f0ac9
Berdnyk-O <sacha.berdnuk@gmail.com>
2024-05-21 13:44:41 +0300

    add a textbox to the assets page search by name

--- CoinTracker/ViewModels/AssetsViewModel.cs
+++ CoinTracker/ViewModels/AssetsViewModel.cs
@@ -7,8 +7,31 @@ namespace CoinTracker.ViewModels
     public class AssetsViewModel : ViewModelBase
     {
         public ObservableCollection<Asset> Assets { get; set; } = null!;
+        private ObservableCollection<Asset> _filteredAssets = new ObservableCollection<Asset>();
+
+        public ObservableCollection<Asset> FilteredAssets
+        {
+            get => _filteredAssets;
+            set
+            {
+                _filteredAssets = value;
+                OnPropertyChanged(nameof(FilteredAssets));
+            }
+        }
 
         private readonly ICoinCapService _coinCapService;
+
+        private string _seachName = string.Empty;
+        public string SearchName
+        { 
+            get => _seachName;
+            set 
+            {
+                _seachName = value;
+                FilterAssets();
+                OnPropertyChanged(nameof(SearchName));    
+            } 
+        }
 
         public AssetsViewModel(ICoinCapService coinCapService)
         {
@@ -19,7 +42,21 @@ namespace CoinTracker.ViewModels
         private async Task LoadAssets()
         {
             Assets = await _coinCapService.GetAssets();
-            OnPropertyChanged(nameof(Assets));
+            FilteredAssets = Assets;
+        }
+
+        private void FilterAssets()
+        {
+            if(string.IsNullOrEmpty(_seachName))
+            {
+                FilteredAssets = Assets;
+            }
+            else
+            {
+                FilteredAssets = new ObservableCollection<Asset>(
+                    Assets.Where(x => x.Name.Contains(
+                        _seachName, StringComparison.InvariantCultureIgnoreCase)));
+            }
         }
     }
 }
